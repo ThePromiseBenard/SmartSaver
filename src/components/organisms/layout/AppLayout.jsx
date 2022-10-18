5d29369bf12fa4ba8852415c1fbf7023386537aa
Promise Benard Augustine <promisebenard1@gmail.com>
2022-10-18 13:01:57 +0100

    Set Up the SEO and Meta tags and arrange the folders for components whilst adding tailwind to the project

---
+++ src/components/organisms/layout/AppLayout.jsx
@@ -0,0 +1,40 @@
+//@ts-nocheck
+import Head from "next/head";
+
+export const AppLayout = ({ children, ...customMeta }) => {
+  const meta = {
+    type: "website",
+    description:
+      "A Financial Supermarket to help you save, earn High Interest, Invest, Transfer &amp; pay Bills. Access Quick Loans &amp; High Yield Savings.",
+    ...customMeta,
+  };
+
+  //  Concatinated this to avoid rendering multiple nodes in the title tag
+
+  const title = `${meta.title && meta.title} | SmartSaver`;
+
+  return (
+    <div style={{ maxWidth: "1440px", marginInline: "auto" }}>
+      <Head>
+        <title>{title}</title>
+        <meta content={meta.description} name="description" />
+        <meta property="og:type" content={meta.type} />
+        <meta
+          property="og:site_name"
+          content=": Reliable Savings &amp; Digital Banking Platform"
+        />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+      </Head>
+      {/* Header component Goes here */}
+
+      <main>{children}</main>
+      {/* Footer component goes here */}
+    </div>
+  );
+};
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { AppLayout } from "../components/organisms/layout/AppLayout";
+
+export default function Home() {
+  return (
+    <AppLayout>
+      <div className="h-screen bg-green-700"></div>;
+    </AppLayout>
+  );
+}
