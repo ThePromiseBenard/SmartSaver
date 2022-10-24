bc455d880c3e94367d8dd96f3d38b74d4d46373b
Promise Benard Augustine <promisebenard1@gmail.com>
2022-10-24 16:38:01 +0100

    fixing lazy load

--- src/components/atoms/SmartImage/SmartImage.jsx
+++ src/components/atoms/SmartImage/SmartImage.jsx
@@ -1,6 +1,6 @@
 import Image from "next/future/image";
 const css = { maxWidth: "100%", height: "auto" };
 
-export const SmartImage = ({ src, alt }) => {
-  return <Image src={src} alt={alt} style={css} />;
+export const SmartImage = ({ src, alt, priority = false }) => {
+  return <Image src={src} alt={alt} priority={priority} style={css} />;
 };
