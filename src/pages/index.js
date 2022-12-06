170a7026cb8a9868637a97e5b5a35a72434818ac
Promise Benard Augustine <promisebenard1@gmail.com>
2022-12-06 22:27:07 +0100

    Added the write up for coming soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 const ComingSoon = () => {
   return (
-    <div className="h-screen flex items-center justify-center text-white bg-green-700 text-5xl">
-      coming Soon
+    <div className="h-screen flex items-center justify-center text-white bg-black/70 text-center">
+      <div className="max-w-7xl space-y-8">
+        <div className="space-y-2">
+          <p className="font-medium text-2xl">Our Website is</p>
+          <h1 className="font-bold text-6xl uppercase">coming soon</h1>
+        </div>
+
+        <p className="text-lg max-w-xl mx-auto">
+          Our website is under construction but we are ready to go! Want to know
+          more about usor just to say hello? Drop us your email details and we
+          will get back as soon as possible.
+        </p>
+
+        <p className="text-lg px-64">
+          Then to access our old website click here:{" "}
+          <Link className=" text-smartOrange" href="https://save.smartsaver.ng">
+            save.smartsaver.ng
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
